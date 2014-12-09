a601e46a61a778219380ee4db5176f14c92a5221
Tim Potter <Tim@Tims-MacBook-Air.local>
2014-12-09 20:15:16 +1100

    Initial Commit

---
+++ MinecraftBingoTests/MinecraftBingoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MinecraftBingoTests.swift
+//  MinecraftBingoTests
+//
+//  Created by Tim Potter on 9/12/2014.
+//  Copyright (c) 2014 Potter Technologies. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MinecraftBingoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
